605040245511f3cabe3fd06e8849b840fb550ab5
Lech Wilczyński <lech.wilczynski@gmail.com>
2015-12-20 23:38:39 +0100

    Removed functions from global scope
    compare functions as private methods

--- bitcurex.php
+++ bitcurex.php
@@ -1,18 +1,5 @@
-<?
-
-function cmpDESC($a, $b) {
-    if ($a[0] == $b[0])
-        return 0;
-    return ($a[0] < $b[0]) ? -1 : 1;
-}
-
-function cmpASC($a, $b) {
-    if ($a[0] == $b[0])
-        return 0;
-    return ($a[0] > $b[0]) ? -1 : 1;
-}
-
-Class Bitcurex {
+<?php
+class Bitcurex {
 
     private $bitcurexUrl = 'https://api.bitcurex.com/v2/';
     private $apiKey;
@@ -21,6 +8,18 @@ Class Bitcurex {
     public function __construct($apiKey = '', $secretKey = '') {
         $this->apiKey = $apiKey;
         $this->secretKey = $secretKey;
+    }
+    
+    private function cmpDESC($a, $b) {
+        if ($a[0] == $b[0])
+            return 0;
+        return ($a[0] < $b[0]) ? -1 : 1;
+    }
+    
+    private function cmpASC($a, $b) {
+        if ($a[0] == $b[0])
+            return 0;
+        return ($a[0] > $b[0]) ? -1 : 1;
     }
 
     private function _getData($url = '', $type = 'GET', $array = null) {
@@ -96,8 +95,8 @@ Class Bitcurex {
     public function orderBook($market = 'pln', $depth = 100) {
         $url = $market . '/' . $depth . '/orderbook';
         $data = $this->_getData($url);
-        usort($data->bids, "cmpASC");
-        usort($data->asks, "cmpDESC");
+        usort($data->bids, array($this, "cmpASC"));
+        usort($data->asks, array($this, "cmpDESC"));
         return $data;
     }
 
